cb4eb1f54bbfbacb2385336ce190f7f0c0182f4e
Gunkhair5 <gunkhair5@gmail.com>
2021-01-21 16:37:53 +0700

    Activity Added

--- app/src/main/java/com/gloriesstudio/AD340_1/MainActivity.kt
+++ app/src/main/java/com/gloriesstudio/AD340_1/MainActivity.kt
@@ -2,11 +2,27 @@ package com.gloriesstudio.AD340_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val zipcodeEditText: EditText = findViewById(R.id.zipcodeEditText)
+        val enterButton: Button = findViewById(R.id.enterButton)
+
+        enterButton.setOnClickListener {
+            val zipcode: String = zipcodeEditText.text.toString()
+            if(zipcode.length != 5){
+                /*Toast.makeText(this, "Zipcode Invalid", Toast.LENGTH_SHORT).show()*/
+                Toast.makeText(this, R.string.invalid_zipcode, Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     // region setup methods
